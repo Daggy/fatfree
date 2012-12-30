04ec95e24f06675eeec791473aec8f07294cbd01
Bong Cosca <bong.cosca@yahoo.com>
2012-12-30 11:32:06 +0800

    Update dir path

--- app/pingback2.php
+++ app/pingback2.php
@@ -6,7 +6,8 @@ class Pingback2 {
 
 	function get($f3) {
 		if ($f3->exists('GET.page'))
-			echo \View::instance()->render($f3->get('GET.page').'.htm');
+			echo \View::instance()->render(
+				'pingback/'.$f3->get('GET.page').'.htm');
 		die;
 	}
 
