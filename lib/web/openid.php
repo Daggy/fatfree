7596b013b3dd8635ec8f7f2cb4218fd264ea7bd7
Bong Cosca <bong.cosca@yahoo.com>
2013-01-02 16:27:18 +0800

    Simplify regex

--- lib/web/openid.php
+++ lib/web/openid.php
@@ -75,12 +75,12 @@ class OpenID extends \Magic {
 			// Parse document
 			while ($ptr<$len)
 				if (preg_match(
-					'/^<link\b((?:\s+\w+s*=\s*'.
-					'(?:"(?:.+?)"|\'(?:.+?)\'))*)\s*\/?>/is',
+					'/^<link\b((?: +\w+s*= *'.
+					'(?:"(?:.+?)"|\'(?:.+?)\'))*) *\/?>/is',
 					substr($req['body'],$ptr),$parts)) {
 					if ($parts[1]) {
 						// Process attributes
-						preg_match_all('/\s+(rel|href)\s*=\s*'.
+						preg_match_all('/\b(rel|href) *= *'.
 							'(?:"(.+?)"|\'(.+?)\')/s',$parts[1],$attr,
 							PREG_SET_ORDER);
 						$node=array();
